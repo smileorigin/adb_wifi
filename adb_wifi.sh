3696c4ceb20adb746f312552fd13919d20b74a5b
smileorigin <smileorigin@foxmail.com>
2019-12-19 14:28:13 +0800

    feat: add a second method to obtain IP

--- adb_wifi.sh
+++ adb_wifi.sh
@@ -4,14 +4,17 @@ port="5555"
 # disconnect existing adb connections first
 adb disconnect
 devices=`adb devices|grep -w device`
-device_count=${#devices}
 
-if [ $device_count != 0 ]; then
+if [ -n "$devices" ]; then
     # get the phone ip
     ip=`adb shell ifconfig wlan0|grep -w inet|awk '{print $2}'|awk -F ':' '{print $2}'`
-    ip_len=${#ip}
 
-    if [ $ip_len != 0 ]; then
+    if [ -z "$ip" ]; then
+        echo "Use the second method to get the ip"
+        ip=`adb shell ip addr show | grep wlan0 | grep -w inet | awk -F '/' '{print $1}' | awk -F ' ' '{print $2}'`
+    fi
+
+    if [ -n "$ip" ]; then
         result=`adb tcpip ${port}`
         result=`adb connect ${ip}`
         echo $result
